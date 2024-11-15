5f410dac8c94ee2ee03033bc856eaca918cf8e88
fj <1655034921@qq.com>
2024-11-15 16:02:12 +0800

    提交

--- javaScript/2.javaScript语言基础/js/base.js
+++ javaScript/2.javaScript语言基础/js/base.js
@@ -1,33 +1,89 @@
-let a = 112;
+// let a = 112;
 
-console.log(a);
-
-
-test = false;
-
-if(test){
-    console.log("true");
-}else{
-    console.log(test+typeof(a));
-}
+// console.log(a);
 
 
-function foo1(){
-    console.log("年龄是"+age);
-    var age = 23;
+// test = false;
 
-}
+// if(test){
+//     console.log("true");
+// }else{
+//     console.log(test+typeof(a));
+// }
 
-//等同于
-function foo2(){
+
+// function foo1(){
+//     console.log("年龄是"+age);
+//     var age = 23;
+
+// }
+
+// //等同于
+// function foo2(){
    
-    var age ;
-    console.log(age);
-    age = 23;
+//     var age ;
+//     console.log(age);
+//     age = 23;
 
-}
+// }
 
-//也就是把所有变量声明都拉到函数作用域的顶部
+// //也就是把所有变量声明都拉到函数作用域的顶部
 
-foo1();
-foo2();
+// foo1();
+// foo2();
+
+
+// {
+//     var v = 111444;
+//     console.log(v);
+//     let b= 111;
+//     console.log(b);
+// }
+
+        // function test(){
+        //    message ='全局变量';
+        // }
+        // console.log(message);
+        // test();
+        // console.log(message);
+
+
+        // var message = '22' ,found = '3';
+
+        // console.log(message+found);
+
+
+
+        // if(true){
+        //     var  message = '343';
+        //     let found  = '2322';
+    
+        // }
+        // console.log(message);
+        
+
+        // var name;
+        // var name;
+
+
+
+        // for(var i =0;i<5;i++){
+        //     console.log(i);
+        // }
+
+/* 
+        for (var i = 0; i < 5; ++i) {
+            setTimeout(() => console.log(i), 0)
+        }
+        // 你可能以为会输出0、1、2、3、4
+        // 实际上会输出5、5、5、5、5
+
+        for (let i = 0; i < 5; ++i) {
+            setTimeout(() => console.log(i), 0)
+         */
+
+        
+        console.log(names);
+        var names ='wqm' ;
+
+        
